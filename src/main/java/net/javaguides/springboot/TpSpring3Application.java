8dbb155f203f69979a822f8ebf42816e1162671d
DESKTOP-0FE37AA\eyach <eyachaabeni2000@gmail.com>
2023-12-21 13:01:00 +0100

    tp3

---
+++ src/main/java/net/javaguides/springboot/TpSpring3Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpring3Application.class, args);
+	}
+
+}
